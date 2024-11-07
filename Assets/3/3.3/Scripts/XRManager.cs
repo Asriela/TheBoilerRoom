884e53809558f7625e81fa174d1eff8c10df53fc
Asriela <45428716+Asriela@users.noreply.github.com>
2024-11-07 19:00:19 +0200

    5 Basic VR scene setup version 2
    1-added the XR Origin from the starter assets sample of the XR Interaction Toolkit
    as opposed to the standard one in the Hierarchy objects menu.
    2-Added an XR canvas.

--- Assets/3/3.3/Scripts/XRManager.cs
+++ Assets/3/3.3/Scripts/XRManager.cs
@@ -1,18 +1,49 @@
 using UnityEngine;
+using UnityEngine.XR.Management;
 using UnityEngine.XR;
+
 public class XRManager : MonoBehaviour
 {
-    private InputDevice leftController;
-    private InputDevice rightController;
+
+
 
     void Start()
     {
-        // Start the XR Session (simulated here for the example)
-      //  StartXRSession();
-    
-        // Initialize input devices for both hands
-        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        // Start XR session
+        StartXRSession();
     }
 
+    void Update()
+    {
+        
+        // Handle input from left hand controller
+        InputDevice leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        var value= leftHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isPressed);
+        if (value )
+        {
+            Debug.Log("Primary button on left controller is pressed.");
+        }
+    }
+
+    void StartXRSession()
+    {
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+        }
+
+        if (!XRGeneralSettings.Instance.Manager.isInitializationComplete)
+        {
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            Debug.Log("XR session started.");
+        }
+    }
+
+    void StopXRSession()
+    {
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Debug.Log("XR session stopped.");
+    }
 }
+
